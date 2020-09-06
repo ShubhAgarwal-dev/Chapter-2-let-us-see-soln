9618fa7b7e5ab5ce003ee7b533db61bd58ec0b6b
Shubh Agarwal <sazorinos1111@gmail.com>
2020-09-06 19:20:54 +0530

    Just exploring some math.h command

---
+++ p1.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include "stdio.h"
+int main()
+{
+  /* Power function */
+  int a;
+  a=pow(9,5);
+  printf(" 9 to th power 5 is %d\n",a);
+  return 0;
+}
---
+++ p2.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+  /* trying mathematical function */
+  //its a comment toooooooooooooo
+  float q,w,e,r,t,y,u;
+  q=sin(90);
+  w=cos(90);
+  e=sqrt(90);
+  r=tan(90);
+  t=pow(4,2);
+  y=q+w+e/t;
+  u=(q+w+e)/t;
+  printf("%f %f %f %f %f %f %f\n",q,w,e,r,t,y,u);
+  return 0;
+}
