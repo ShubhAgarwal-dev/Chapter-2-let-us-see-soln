b48a15907fd52988607db8c3f86034b600319f5b
Shubh Agarwal <sazorinos1111@gmail.com>
2020-09-07 09:00:51 +0530

    Question 6 of attempt the following

---
+++ q5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("enter a five digit number:");
+	int q,w;
+	scanf("%d",&q);
+	w=q+11111;
+	printf("new number after adding one to each of its digit is %d\n",w);
+	return 0;
+}
